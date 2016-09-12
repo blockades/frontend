22aec07f1e89a819c241bf0b17640ea7386a3234
Dan Hassan <daniel.san@dyne.org>
2016-09-12 22:17:49 +1000

    updating project name and github repo

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -10,7 +10,9 @@ export default class Home extends Component {
       <div className={styles.home + ' container'}>
         <h1>Home</h1>
         <Helmet title="Home"/>
-        Hello, world!
+        <p>openblockchain.info is a web application which allows simple statistical analysis of potential ‘social and cultural’ uses of the Bitcoin blockchain by an untrained user. The user can navigate various available charts, selecting more fine-grained parameters for display on the charts. She can also zoom into information of individual Transactions and Blocks and can view statistics relating to the entire blockchain, or over user defined time-spans.</p>
+
+        <p>An advanced user is able to deploy the entire application by harnessing the Docker setup allowing her to run her own cluster locally or in the cloud.</p>
       </div>
     );
   }
